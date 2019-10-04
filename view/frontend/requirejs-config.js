7cce2a6e1964597e1f01ec6f92dd2d9a1cf82efa
siemsonSiemson <adam.from.fishbane@gmail.com>
2019-10-04 16:00:53 +0200

    added map for overwritten file

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -7,6 +7,11 @@ var config = {
             'Adyen_Payment/js/view/payment/method-renderer/adyen-cc-method': {
                 'Riskified_Decider/js/view/payment/method-renderer/adyen-cc-method': true
             },
+        },
+        map: {
+            '*': {
+                'Adyen_Payment/js/view/payment/method-renderer/adyen-cc-method':'Riskified_Decider/js/view/payment/method-renderer/adyen-cc-method'
+            }
         }
     }
 };
