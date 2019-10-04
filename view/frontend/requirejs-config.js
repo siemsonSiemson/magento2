af10c2f85a355c797ea8a1d0def3bfafb78c459d
siemsonSiemson <adam.from.fishbane@gmail.com>
2019-10-04 16:43:48 +0200

    proper overriding state

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -4,9 +4,6 @@ var config = {
             'Magento_Braintree/js/view/payment/validator-handler': {
                 'Riskified_Decider/js/view/payment/braintree/validator-handler-mixin': true
             },
-            'Adyen_Payment/js/view/payment/method-renderer/adyen-cc-method': {
-                'Riskified_Decider/js/view/payment/method-renderer/adyen-cc-method': true
-            },
         },
         map: {
             '*': {
