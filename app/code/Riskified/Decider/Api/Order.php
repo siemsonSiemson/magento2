d2875c07f5fc0d6b45c12d536c7432e65ef7763a
Nachshon <nahshf@gmail.com>
2019-12-12 17:08:08 +0100

    added order model refund field

--- app/code/Riskified/Decider/Api/Order.php
+++ app/code/Riskified/Decider/Api/Order.php
@@ -227,9 +227,11 @@ class Order
         $order->line_items = $this->_orderHelper->getLineItems();
         $order->shipping_lines = $this->_orderHelper->getShippingLines();
         $refundDetails = $this->_orderHelper->getRefundDetails();
-        
+
         if(empty($refundDetails) != 1){
             $order->refunds = $refundDetails;
+        }else{
+            $order->refunds = 'no refunds';
         }
 
         if (!$this->_backendAuthSession->isLoggedIn()) {
